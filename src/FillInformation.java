85c6c33e3f9c90e09e9bb22007cff6783b285992
redzwantaufek <redzwantaufek2@gmail.com>
2022-06-16 01:41:23 +0800

    initial commit

--- src/FillInformation.java
+++ src/FillInformation.java
@@ -18,11 +18,17 @@ import javax.swing.JList;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.JButton;
 
 public class FillInformation extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textSeat;
+	private JTextField textField;
+	private JTextField textHallType;
+	private JTextField textName;
 
 	/**
 	 * Launch the application.
@@ -44,18 +50,20 @@ public class FillInformation extends JFrame {
 	 * Create the frame.
 	 */
 	public FillInformation() {
+		setTitle("Diamond Screen Cinema (DSC)");
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Gold Line art Video Camera for Movie Cinema Production Logo (200 \u00D7 200 px).png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1363, 607);
+		setBounds(100, 100, 1364, 609);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 128, 128));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblCinmaIcon = new JLabel("");
-		lblCinmaIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Gold Line art Video Camera for Movie Cinema Production Logo (200 \u00D7 200 px).png"));
-		lblCinmaIcon.setBounds(169, 11, 200, 129);
+		lblCinmaIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Gold Line art Video Camera for Movie Cinema Production Logo 2(200 \u00D7 200 px) (500 \u00D7 129 px).png"));
+		lblCinmaIcon.setBounds(22, 11, 500, 129);
 		contentPane.add(lblCinmaIcon);
 		
 		JLabel lblSeatIcon = new JLabel("");
@@ -70,89 +78,172 @@ public class FillInformation extends JFrame {
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
-		separator_1.setBounds(947, 0, 8, 574);
+		separator_1.setBounds(940, 0, 8, 574);
 		contentPane.add(separator_1);
 		
 		JLabel lblTitle = new JLabel("PLEASE FILL THE INFORMATION");
 		lblTitle.setForeground(new Color(255, 255, 255));
 		lblTitle.setBackground(new Color(255, 255, 255));
-		lblTitle.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblTitle.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setBounds(994, 11, 325, 14);
 		contentPane.add(lblTitle);
 		
 		JLabel lblSeatNumber = new JLabel("SEAT NUMBER");
 		lblSeatNumber.setForeground(new Color(255, 255, 255));
-		lblSeatNumber.setFont(new Font("Arial", Font.PLAIN, 13));
-		lblSeatNumber.setBounds(965, 47, 160, 14);
+		lblSeatNumber.setFont(new Font("Arial", Font.BOLD, 13));
+		lblSeatNumber.setBounds(965, 110, 160, 14);
 		contentPane.add(lblSeatNumber);
 		
 		JLabel lblAdult = new JLabel("ADULT");
 		lblAdult.setForeground(new Color(255, 255, 255));
-		lblAdult.setFont(new Font("Arial", Font.PLAIN, 13));
-		lblAdult.setBounds(965, 80, 160, 14);
+		lblAdult.setFont(new Font("Arial", Font.BOLD, 13));
+		lblAdult.setBounds(965, 147, 160, 14);
 		contentPane.add(lblAdult);
 		
 		JLabel lblChildren = new JLabel("CHILDREN");
 		lblChildren.setForeground(new Color(255, 255, 255));
-		lblChildren.setFont(new Font("Arial", Font.PLAIN, 13));
-		lblChildren.setBounds(965, 115, 160, 14);
+		lblChildren.setFont(new Font("Arial", Font.BOLD, 13));
+		lblChildren.setBounds(965, 182, 160, 14);
 		contentPane.add(lblChildren);
 		
 		JLabel lblMembership = new JLabel("MEMBERSHIP");
-		lblMembership.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblMembership.setFont(new Font("Arial", Font.BOLD, 13));
 		lblMembership.setForeground(new Color(255, 255, 255));
-		lblMembership.setBounds(965, 151, 160, 14);
+		lblMembership.setBounds(965, 218, 160, 14);
 		contentPane.add(lblMembership);
 		
 		JLabel lblFnB = new JLabel("FOOD AND BEVERAGES");
-		lblFnB.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblFnB.setFont(new Font("Arial", Font.BOLD, 13));
 		lblFnB.setForeground(new Color(255, 255, 255));
-		lblFnB.setBounds(965, 227, 160, 14);
+		lblFnB.setBounds(965, 294, 160, 14);
 		contentPane.add(lblFnB);
 		
 		textSeat = new JTextField();
+		textSeat.setBackground(new Color(255, 255, 255));
 		textSeat.setFont(new Font("Arial", Font.PLAIN, 11));
-		textSeat.setBounds(1177, 44, 142, 20);
+		textSeat.setBounds(1177, 108, 142, 20);
 		contentPane.add(textSeat);
 		textSeat.setColumns(10);
 		
 		JSpinner spinnerAdult = new JSpinner();
+		spinnerAdult.setBackground(new Color(255, 255, 255));
 		spinnerAdult.setFont(new Font("Arial", Font.PLAIN, 11));
-		spinnerAdult.setBounds(1177, 77, 142, 20);
+		spinnerAdult.setBounds(1177, 141, 142, 20);
 		contentPane.add(spinnerAdult);
 		
 		JSpinner spinnerChildren = new JSpinner();
+		spinnerChildren.setBackground(new Color(255, 255, 255));
 		spinnerChildren.setFont(new Font("Arial", Font.PLAIN, 11));
-		spinnerChildren.setBounds(1177, 112, 142, 20);
+		spinnerChildren.setBounds(1177, 176, 142, 20);
 		contentPane.add(spinnerChildren);
 		
 		JRadioButton rdbtnMembership = new JRadioButton("Membership");
+		rdbtnMembership.setBackground(new Color(255, 255, 255));
 		rdbtnMembership.setFont(new Font("Arial", Font.PLAIN, 11));
-		rdbtnMembership.setBounds(1177, 149, 142, 19);
+		rdbtnMembership.setBounds(1177, 213, 142, 19);
 		contentPane.add(rdbtnMembership);
 		
 		JRadioButton rdbtnNonMembership = new JRadioButton("Non-Membership");
+		rdbtnNonMembership.setBackground(new Color(255, 255, 255));
 		rdbtnNonMembership.setFont(new Font("Arial", Font.PLAIN, 11));
-		rdbtnNonMembership.setBounds(1177, 186, 142, 19);
+		rdbtnNonMembership.setBounds(1177, 250, 142, 19);
 		contentPane.add(rdbtnNonMembership);
 		
 		JComboBox comboBoxFnB1 = new JComboBox();
+		comboBoxFnB1.setBackground(new Color(255, 255, 255));
 		comboBoxFnB1.setFont(new Font("Arial", Font.PLAIN, 11));
 		comboBoxFnB1.setModel(new DefaultComboBoxModel(new String[] {"1. Nasi Kerabu", "2. Popcorn", "3. Soda Drink", "4.Hot Dog"}));
-		comboBoxFnB1.setBounds(1177, 225, 142, 19);
+		comboBoxFnB1.setBounds(1177, 289, 142, 19);
 		contentPane.add(comboBoxFnB1);
 		
 		JComboBox comboBoxFnB2 = new JComboBox();
+		comboBoxFnB2.setBackground(new Color(255, 255, 255));
 		comboBoxFnB2.setModel(new DefaultComboBoxModel(new String[] {"1. Nasi Kerabu", "2. Popcorn", "3. Soda Drink", "4.Hot Dog"}));
 		comboBoxFnB2.setFont(new Font("Arial", Font.PLAIN, 11));
-		comboBoxFnB2.setBounds(1177, 264, 142, 19);
+		comboBoxFnB2.setBounds(1177, 412, 142, 19);
 		contentPane.add(comboBoxFnB2);
 		
 		JComboBox comboBoxFnB3 = new JComboBox();
+		comboBoxFnB3.setBackground(new Color(255, 255, 255));
 		comboBoxFnB3.setFont(new Font("Arial", Font.PLAIN, 11));
 		comboBoxFnB3.setModel(new DefaultComboBoxModel(new String[] {"1. Nasi Kerabu", "2. Popcorn", "3. Soda Drink", "4.Hot Dog"}));
-		comboBoxFnB3.setBounds(1177, 303, 142, 19);
+		comboBoxFnB3.setBounds(1177, 350, 142, 19);
 		contentPane.add(comboBoxFnB3);
+		
+		JSpinner spinnerFnB1 = new JSpinner();
+		spinnerFnB1.setBackground(new Color(255, 255, 255));
+		spinnerFnB1.setFont(new Font("Arial", Font.PLAIN, 11));
+		spinnerFnB1.setBounds(1177, 319, 142, 20);
+		contentPane.add(spinnerFnB1);
+		
+		JSpinner spinnerFnB2 = new JSpinner();
+		spinnerFnB2.setBackground(new Color(255, 255, 255));
+		spinnerFnB2.setFont(new Font("Arial", Font.PLAIN, 11));
+		spinnerFnB2.setBounds(1177, 381, 142, 20);
+		contentPane.add(spinnerFnB2);
+		
+		JSpinner spinnerFnB3 = new JSpinner();
+		spinnerFnB3.setBackground(new Color(255, 255, 255));
+		spinnerFnB3.setFont(new Font("Arial", Font.PLAIN, 11));
+		spinnerFnB3.setBounds(1177, 442, 142, 20);
+		contentPane.add(spinnerFnB3);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(965, 508, 160, 10);
+		contentPane.add(separator_2);
+		
+		JSeparator separator_2_1 = new JSeparator();
+		separator_2_1.setBounds(965, 547, 160, 10);
+		contentPane.add(separator_2_1);
+		
+		textField = new JTextField();
+		textField.setBackground(new Color(255, 255, 255));
+		textField.setBounds(965, 518, 160, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnProceed = new JButton("Proceed");
+		btnProceed.setBackground(new Color(255, 255, 255));
+		btnProceed.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnProceed.setBounds(1177, 517, 142, 23);
+		contentPane.add(btnProceed);
+		
+		JLabel lblPriceIcon = new JLabel("");
+		lblPriceIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Green Minimalist Price List Instagram Post.png"));
+		lblPriceIcon.setBounds(560, 11, 367, 546);
+		contentPane.add(lblPriceIcon);
+		
+		JLabel lblCinemaType = new JLabel("CINEMA TYPE");
+		lblCinemaType.setForeground(Color.WHITE);
+		lblCinemaType.setFont(new Font("Arial", Font.BOLD, 13));
+		lblCinemaType.setBounds(965, 76, 160, 14);
+		contentPane.add(lblCinemaType);
+		
+		textHallType = new JTextField();
+		textHallType.setBackground(new Color(255, 255, 255));
+		textHallType.setFont(new Font("Arial", Font.PLAIN, 11));
+		textHallType.setColumns(10);
+		textHallType.setBounds(1177, 74, 142, 20);
+		contentPane.add(textHallType);
+		
+		JLabel lblName = new JLabel("NAME");
+		lblName.setForeground(Color.WHITE);
+		lblName.setFont(new Font("Arial", Font.BOLD, 13));
+		lblName.setBounds(965, 45, 160, 14);
+		contentPane.add(lblName);
+		
+		textName = new JTextField();
+		textName.setFont(new Font("Arial", Font.PLAIN, 11));
+		textName.setColumns(10);
+		textName.setBackground(Color.WHITE);
+		textName.setBounds(1177, 43, 142, 20);
+		contentPane.add(textName);
+		
+		JLabel lblTotal = new JLabel("TOTAL:");
+		lblTotal.setForeground(Color.WHITE);
+		lblTotal.setFont(new Font("Arial", Font.BOLD, 13));
+		lblTotal.setBounds(965, 483, 160, 14);
+		contentPane.add(lblTotal);
 	}
 }
---
+++ src/WelcomePage.java
@@ -0,0 +1,202 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JTabbedPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+
+public class WelcomePage extends JFrame {
+
+	
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WelcomePage frame = new WelcomePage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WelcomePage() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Gold Line art Video Camera for Movie Cinema Production Logo (200 \u00D7 200 px).png"));
+		setTitle("DIAMOND SCREEN CINEMA (DSC)");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1364, 609);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 128, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogoIconWelcome = new JLabel("");
+		lblLogoIconWelcome.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\DSC Cinema Logo Long (950 \u00D7 107 px) dolby imax.png"));
+		lblLogoIconWelcome.setBounds(388, 11, 950, 107);
+		contentPane.add(lblLogoIconWelcome);
+		
+		JPanel panelNowShowing = new JPanel();
+		panelNowShowing.setBackground(new Color(0, 102, 102));
+		panelNowShowing.setBorder(new TitledBorder(null, "NOW SHOWING", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		panelNowShowing.setBounds(10, 11, 368, 548);
+		contentPane.add(panelNowShowing);
+		panelNowShowing.setLayout(null);
+		
+		JLabel lblTopGunIcon = new JLabel("");
+		lblTopGunIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\TOPGUN POSTER kecik.jpg"));
+		lblTopGunIcon.setBounds(10, 21, 159, 222);
+		panelNowShowing.add(lblTopGunIcon);
+		
+		JLabel lblDrStrangeIcon = new JLabel("");
+		lblDrStrangeIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\FQWWNqgVEAUX1Uz kecik.jpg"));
+		lblDrStrangeIcon.setBounds(199, 21, 152, 222);
+		panelNowShowing.add(lblDrStrangeIcon);
+		
+		JLabel lblJurassicIcon = new JLabel("");
+		lblJurassicIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\nima-neemz-nakhshab-dominion-final kecik.jpg"));
+		lblJurassicIcon.setBounds(199, 288, 152, 222);
+		panelNowShowing.add(lblJurassicIcon);
+		
+		JLabel lblSonicIcon = new JLabel("");
+		lblSonicIcon.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\MV5BMGI1NjA1MjUtNGQxNC00NDYyLThjODgtZjFkZjQ4OGM0NDc5XkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_FMjpg_UX1000_Sonic.jpg"));
+		lblSonicIcon.setBounds(10, 288, 152, 222);
+		panelNowShowing.add(lblSonicIcon);
+		
+		JLabel lblTopGunLabel = new JLabel("Top Gun: Maverick");
+		lblTopGunLabel.setForeground(Color.WHITE);
+		lblTopGunLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTopGunLabel.setFont(new Font("Arial", Font.BOLD, 11));
+		lblTopGunLabel.setBounds(10, 254, 152, 14);
+		panelNowShowing.add(lblTopGunLabel);
+		
+		JLabel lblSonic2Label = new JLabel("Sonic The Heghedog 2");
+		lblSonic2Label.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSonic2Label.setForeground(Color.WHITE);
+		lblSonic2Label.setFont(new Font("Arial", Font.BOLD, 11));
+		lblSonic2Label.setBounds(10, 521, 152, 14);
+		panelNowShowing.add(lblSonic2Label);
+		
+		JLabel lblJurassicLabel = new JLabel("Jurassic World Dominion");
+		lblJurassicLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJurassicLabel.setForeground(Color.WHITE);
+		lblJurassicLabel.setFont(new Font("Arial", Font.BOLD, 11));
+		lblJurassicLabel.setBounds(199, 521, 152, 14);
+		panelNowShowing.add(lblJurassicLabel);
+		
+		JLabel lblDrStrange2Label = new JLabel("Doctor Strange: 2");
+		lblDrStrange2Label.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDrStrange2Label.setForeground(Color.WHITE);
+		lblDrStrange2Label.setFont(new Font("Arial", Font.BOLD, 11));
+		lblDrStrange2Label.setBounds(199, 254, 152, 14);
+		panelNowShowing.add(lblDrStrange2Label);
+		
+		JPanel panelsShowtime = new JPanel();
+		panelsShowtime.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "SHOWTIME", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(255, 255, 255)));
+		panelsShowtime.setBackground(new Color(0, 102, 102));
+		panelsShowtime.setBounds(388, 129, 656, 430);
+		contentPane.add(panelsShowtime);
+		panelsShowtime.setLayout(null);
+		
+		JLabel lblCinemaType = new JLabel("CINEMA TYPE");
+		lblCinemaType.setForeground(new Color(255, 255, 255));
+		lblCinemaType.setFont(new Font("Arial", Font.BOLD, 14));
+		lblCinemaType.setBounds(29, 22, 113, 30);
+		panelsShowtime.add(lblCinemaType);
+		
+		JComboBox comboBoxCinemaType = new JComboBox();
+		comboBoxCinemaType.setBackground(new Color(255, 255, 255));
+		comboBoxCinemaType.setModel(new DefaultComboBoxModel(new String[] {"1. Classic", "2. Dolby Cinema", "3. IMAX Cinema", "4. Deluxe", "5. Luxury"}));
+		comboBoxCinemaType.setFont(new Font("Arial", Font.BOLD, 14));
+		comboBoxCinemaType.setBounds(217, 27, 177, 22);
+		panelsShowtime.add(comboBoxCinemaType);
+		
+		JLabel lblMovie = new JLabel("MOVIE");
+		lblMovie.setForeground(Color.WHITE);
+		lblMovie.setFont(new Font("Arial", Font.BOLD, 14));
+		lblMovie.setBounds(29, 73, 113, 30);
+		panelsShowtime.add(lblMovie);
+		
+		JLabel lblDate = new JLabel("DATE");
+		lblDate.setForeground(Color.WHITE);
+		lblDate.setFont(new Font("Arial", Font.BOLD, 14));
+		lblDate.setBounds(29, 123, 113, 30);
+		panelsShowtime.add(lblDate);
+		
+		JLabel lblTime = new JLabel("TIME");
+		lblTime.setForeground(Color.WHITE);
+		lblTime.setFont(new Font("Arial", Font.BOLD, 14));
+		lblTime.setBounds(29, 174, 113, 30);
+		panelsShowtime.add(lblTime);
+		
+		JComboBox comboBoxMovie = new JComboBox();
+		comboBoxMovie.setModel(new DefaultComboBoxModel(new String[] {"1. Top Gun Maverick", "2. Doctor Strange: 2", "3. Sonic The Heghedog 2", "4. Jurassic World Dominion"}));
+		comboBoxMovie.setFont(new Font("Arial", Font.BOLD, 14));
+		comboBoxMovie.setBackground(Color.WHITE);
+		comboBoxMovie.setBounds(217, 78, 177, 22);
+		panelsShowtime.add(comboBoxMovie);
+		
+		JComboBox comboBoxDate = new JComboBox();
+		comboBoxDate.setModel(new DefaultComboBoxModel(new String[] {"20/6/2022", "21/6/2022", "22/6/2022", "23/6/2022", "24/6/2022", "25/6/2022", "26/6/2022", "27/6/2022"}));
+		comboBoxDate.setFont(new Font("Arial", Font.BOLD, 14));
+		comboBoxDate.setBackground(Color.WHITE);
+		comboBoxDate.setBounds(217, 128, 177, 22);
+		panelsShowtime.add(comboBoxDate);
+		
+		JComboBox comboBoxTime = new JComboBox();
+		comboBoxTime.setMaximumRowCount(10);
+		comboBoxTime.setModel(new DefaultComboBoxModel(new String[] {"11:00", "01:30", "03:20", "05:40", "07:30", "08:10", "09:20", "10:10", "11:30", "12:10"}));
+		comboBoxTime.setFont(new Font("Arial", Font.BOLD, 14));
+		comboBoxTime.setBackground(Color.WHITE);
+		comboBoxTime.setBounds(217, 179, 177, 22);
+		panelsShowtime.add(comboBoxTime);
+		
+		JLabel lblShowtimeTable = new JLabel("");
+		lblShowtimeTable.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\Comparison Table List Infographic Gantt Chart Graph (234 \u00D7 408 px).png"));
+		lblShowtimeTable.setBounds(412, 15, 234, 404);
+		panelsShowtime.add(lblShowtimeTable);
+		
+		JButton btnNext = new JButton("NEXT");
+		btnNext.setBounds(217, 231, 177, 23);
+		panelsShowtime.add(btnNext);
+		btnNext.setForeground(new Color(0, 0, 0));
+		btnNext.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		JPanel panelComingSoon = new JPanel();
+		panelComingSoon.setBackground(new Color(0, 102, 102));
+		panelComingSoon.setBorder(new TitledBorder(null, "COMING SOON", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(255, 255, 255)));
+		panelComingSoon.setBounds(1054, 129, 284, 430);
+		contentPane.add(panelComingSoon);
+		panelComingSoon.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\taufe\\Documents\\Lecture\\Semester 2\\Object Oriented Programming(SWC2333)\\Kerja\\Assignment group\\Gambo\\images kecik.jpg"));
+		lblNewLabel.setBounds(10, 25, 264, 394);
+		panelComingSoon.add(lblNewLabel);
+	}
+}
